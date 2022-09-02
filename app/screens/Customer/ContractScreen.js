f9265741fbb5245d5ae0f8f31c53dcaf8dae4edc
Usman-Marwat <us895907a2@gmail.com>
2022-09-02 10:15:13 +0500

    Build the Heading sparator for lists

--- app/screens/Customer/ContractScreen.js
+++ app/screens/Customer/ContractScreen.js
@@ -1,18 +1,60 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { ProfileHeader } from "@freakycoder/react-native-header-view";
+import { Left } from "react-native-component-separator";
 
 import Screen from "../../components/Screen";
+
+const staticData = [
+  {
+    title: "",
+    value: "8,984",
+    unit: "Steps",
+    primaryColor: "#10CFE4",
+    imageSource: require("../../assets/icon.png"),
+  },
+  {
+    title: "",
+    value: "2.6",
+    unit: "Mil",
+    primaryColor: "#c84cf0",
+    imageSource: require("../../assets/icon.png"),
+  },
+  {
+    title: "",
+    value: "9501",
+    unit: "Stoke",
+    primaryColor: "#10E471",
+    imageSource: require("../../assets/icon.png"),
+  },
+];
+
+const imageUrl =
+  "https://images.unsplash.com/photo-1661977597155-1277a81affcd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80";
 
 const ContractScreen = () => {
   return (
     <Screen>
       <ProfileHeader height={70} />
+      <View style={styles.separator}>
+        <Left borderColor="#e0e0e0" color="#616161">
+          House Building
+        </Left>
+      </View>
     </Screen>
   );
 };
 
 export default ContractScreen;
+
+const styles = StyleSheet.create({
+  separator: {
+    paddingLeft: 15,
+    paddingRight: 30,
+    marginVertical: 35,
+    marginBottom: 15,
+  },
+});
 
 /* 
 import RNBounceable from "@freakycoder/react-native-bounceable";
