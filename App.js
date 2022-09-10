236a9e60616995db8d945788ec7f87f04e3c9b46
Usman-Marwat <us895907a2@gmail.com>
2022-09-10 16:22:45 +0500

    Build Flatlist with faker data

--- App.js
+++ App.js
@@ -1,50 +1,103 @@
-import "react-native-gesture-handler";
-import React, { useEffect, useState, useContext } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { StreamChat } from "stream-chat";
+import * as React from "react";
 import {
-  Channel,
-  ChannelList,
-  Chat,
-  MessageInput,
-  MessageList,
-  OverlayProvider,
-} from "stream-chat-expo";
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
+import { faker } from "@faker-js/faker";
 
-import AuthContext from "./contexts/Authentication";
-import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./routes/RootNavigator";
+import Screen from "./app/components/Screen";
 
-const Api_Key = "fnmp8yh8yfe2";
-const client = StreamChat.getInstance(Api_Key);
+faker.seed(10);
+
+const BG_IMG =
+  "https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg?auto=compress&cs=tinysrgb&w=800";
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.datatype.uuid(),
+    image: faker.image.people(640, 480, true),
+    name: faker.name.firstName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
 
 export default function App() {
-  const [selectedChannel, setSelectedChannel] = useState(null);
-  const [userId, setUserId] = useState("");
-
-  useEffect(() => {
-    return () => client.disconnectUser();
-  }, []);
-
   return (
-    <SafeAreaView style={styles.container}>
-      <AuthContext.Provider value={{ userId, setUserId }}>
-        <OverlayProvider>
-          <Chat client={client}>
-            <NavigationContainer>
-              <RootNavigator />
-            </NavigationContainer>
-          </Chat>
-        </OverlayProvider>
-      </AuthContext.Provider>
-    </SafeAreaView>
+    <Screen>
+      <Image
+        source={{ uri: BG_IMG }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={80}
+      />
+      <FlatList
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.itemWrapper}>
+              <Image source={{ uri: item.image }} style={styles.image} />
+              <View style={{ flexShrink: 1 }}>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    opacity: 0.7,
+                  }}
+                >
+                  {item.jobTitle}
+                </Text>
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099cc" }}>
+                  {item.email}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+      <StatusBar hidden />
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  itemWrapper: {
+    flexDirection: "row",
+    padding: SPACING,
+    marginBottom: SPACING,
+    backgroundColor: "rgba(255,255,255,0.8)",
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 20,
+  },
+  image: {
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    borderRadius: AVATAR_SIZE,
+    marginRight: SPACING,
   },
 });
