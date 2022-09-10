fac006f6a248a1b52a864a0bb48d2e765fcc1c47
Usman-Marwat <us895907a2@gmail.com>
2022-09-10 16:29:34 +0500

    Bring employeesList into its own Screen

---
+++ app/screens/Contractor/EmployeesList.js
@@ -0,0 +1,107 @@
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from "react-native";
+
+import React from "react";
+
+const { width, height } = Dimensions.get("screen");
+import { faker } from "@faker-js/faker";
+
+import Screen from "../../components/Screen";
+
+faker.seed(10);
+
+const BG_IMG =
+  "https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg?auto=compress&cs=tinysrgb&w=800";
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.datatype.uuid(),
+    image: faker.image.people(640, 480, true),
+    name: faker.name.firstName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+
+const EmployeesList = () => {
+  return (
+    <Screen>
+      <Image
+        source={{ uri: BG_IMG }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={80}
+      />
+      <FlatList
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.itemWrapper}>
+              <Image source={{ uri: item.image }} style={styles.image} />
+              <View style={{ flexShrink: 1 }}>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    opacity: 0.7,
+                  }}
+                >
+                  {item.jobTitle}
+                </Text>
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099cc" }}>
+                  {item.email}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+      <StatusBar hidden />
+    </Screen>
+  );
+};
+
+export default EmployeesList;
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    flexDirection: "row",
+    padding: SPACING,
+    marginBottom: SPACING,
+    backgroundColor: "rgba(255,255,255,0.8)",
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 20,
+  },
+  image: {
+    width: AVATAR_SIZE,
+    height: AVATAR_SIZE,
+    borderRadius: AVATAR_SIZE,
+    marginRight: SPACING,
+  },
+});
