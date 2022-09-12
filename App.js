17596edb65a9214da43f09f245585dd05c9b22c8
Usman-Marwat <us895907a2@gmail.com>
2022-09-12 16:30:45 +0500

    Create the Starter animation

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
-import {
-  Animated,
-  Button,
-  Dimensions,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { StyleSheet } from "react-native";
 
-import ContractNavigator from "./app/navigation/CustomerNavigation/ContractNavigator";
-import navigationTheme from "./app/navigation/navigationTheme";
-
-import CustomDrawer from "./app/navigation/CustomDrawer";
-import CustomerNavigator from "./app/navigation/CustomerNavigation/CustomerNavigatior";
-import Icon from "./app/components/Icon";
+import BrandAnimation from "./app/start/BrandAnimation";
 
 export default function App() {
-  return <CustomerNavigator />;
+  return <BrandAnimation />;
 }
 
 const styles = StyleSheet.create({
