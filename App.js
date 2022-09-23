0d7cf3efdca4ea1b1143f01ddb4b419b684e6e3e
Usman-Marwat <us895907a2@gmail.com>
2022-09-23 13:28:39 +0500

    Build boilerplate for the new Welcome Screen

--- App.js
+++ App.js
@@ -1,32 +1,10 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 
-import BrandAnimation from "./app/start/BrandAnimation";
-import useApi from "./app/hooks/useApi";
-import cloudinaryApi from "./app/api/cloudinary";
-import ImageInputList from "./app/components/ImageInputList";
-import AppButton from "./app/components/AppButton";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  const { data, error, request: addImage } = useApi(cloudinaryApi.addImage);
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleImage = async () => {
-    const image = imageUris[0];
-    await addImage(image, (prog) => console.log(prog));
-    console.log(error);
-    console.log(data);
-  };
-
-  const appendUris = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-  return (
-    <>
-      <ImageInputList imageUris={imageUris} onAddImage={appendUris} />
-      <AppButton title="Send" onPress={handleImage} />
-    </>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,70 +1,78 @@
-import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
-import React from "react";
-import AppButton from "../components/AppButton";
+import * as React from "react";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+const { width, height } = Dimensions.get("screen");
 
-export default function WelcomeScreen({ navigation }) {
+const bgs = ["#A5BBFF", "#DDBEFE", "#FF63ED", "#B98EFF"];
+const DATA = [
+  {
+    key: "3571572",
+    title: "Multi-lateral intermediate moratorium",
+    description:
+      "I'll back up the multi-byte XSS matrix, that should feed the SCSI WelcomeScreenlication!",
+    image: "https://cdn-icons-png.flaticon.com/512/3571/3571572.png",
+  },
+  {
+    key: "3571747",
+    title: "Automated radical data-warehouse",
+    description:
+      "Use the optical SAS system, then you can navigate the auxiliary alarm!",
+    image: "https://cdn-icons-png.flaticon.com/128/3571/3571747.png",
+  },
+  {
+    key: "3571680",
+    title: "Inverse attitude-oriented system engine",
+    description:
+      "The ADP array is down, compress the online sensor so we can input the HTTP panel!",
+    image: "https://cdn-icons-png.flaticon.com/512/3571/3571680.png",
+  },
+  {
+    key: "3571603",
+    title: "Monitored global data-warehouse",
+    description: "We need to program the open-source IB interface!",
+    image: "https://cdn-icons-png.flaticon.com/512/3571/3571603.png",
+  },
+];
+
+export default function WelcomeScreen() {
   return (
-    <ImageBackground
-      blurRadius={7}
-      style={styles.background}
-      source={require("../assets/background.jpg")}
-    >
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require("../assets/logo-red.png")}
-        ></Image>
-        <Text style={styles.tagLine}> Sell What You Don't Need</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <AppButton title="Login" onPress={() => navigation.navigate("Login")} />
-        <View style={styles.spaceView} />
-        <AppButton
-          title="Register"
-          color="secondary"
-          onPress={() => navigation.navigate("Register")}
-        />
-        <View style={styles.spaceView} />
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <Animated.FlatList
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: width / 2,
+                  height: width / 2.5,
+                  resizeMode: "contain",
+                }}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
-    justifyContent: "flex-end",
+    backgroundColor: "#fff",
     alignItems: "center",
-  },
-  buttonContainer: {
-    paddingHorizontal: 20,
-    width: "100%",
-  },
-  // loginButton: {
-  //   width: "100%",
-  //   height: 70,
-  //   backgroundColor: "#fc5c65",
-  // },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  logoContainer: {
-    position: "absolute",
-    top: 70,
-    alignItems: "center",
-  },
-  // registerButton: {
-  //   width: "100%",
-  //   height: 70,
-  //   backgroundColor: "#4ecdc4",
-  // },
-  spaceView: {
-    height: 17,
-  },
-  tagLine: {
-    fontSize: 25,
-    fontWeight: "600",
-    marginVertical: 10,
+    justifyContent: "center",
   },
 });
