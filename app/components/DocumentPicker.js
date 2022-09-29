d099cfee783222ab749cccedd5aa5c2d5e039170
Usman-Marwat <us895907a2@gmail.com>
2022-09-29 23:11:36 +0500

    Build the whole scrollable document picker

--- app/components/DocumentPicker.js
+++ app/components/DocumentPicker.js
@@ -1,35 +1,79 @@
-import React, { useState, useCallback } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import * as DP from "expo-document-picker";
+import _ from "lodash";
+import niceColors from "nice-color-palettes";
+
+import AppButton from "./AppButton";
+import colors from "../config/colors";
+
+const colorsP = [
+  "#3F5B98",
+  ...niceColors[39],
+  "#FCBE4A",
+  "#FD5963",
+  ...niceColors[8],
+  "#FECBCD",
+  ...niceColors[10].slice(1, 5),
+  ...niceColors[6],
+];
 
 const DocumentPicker = () => {
-  const [fileResponse, setFileResponse] = useState([]);
+  const [filesUris, setFilesUris] = useState([]);
+  const scrollView = useRef();
 
-  const handleDocumentSelection = useCallback(async () => {
+  const handleDocumentSelection = async () => {
     try {
       const response = await DP.getDocumentAsync({
         copyToCacheDirectory: false,
       });
-      console.log(response);
-      setFileResponse(response);
+      if (response.type === "cancel") throw Error("Canceled");
+      const filesUris2 = _.cloneDeep(filesUris);
+      filesUris2.push(response);
+      console.log(filesUris2.length);
+      setFilesUris(filesUris2);
     } catch (err) {
       console.log(err);
     }
-  }, []);
+  };
 
   return (
     <View style={styles.container}>
-      {/* {fileResponse.map((file, index) => (
-        <Text
-          key={index.toString()}
-          style={styles.uri}
-          numberOfLines={1}
-          ellipsizeMode={"middle"}
-        >
-          {file?.uri}
-        </Text>
-      ))} */}
-      <Button title="Select 📑" onPress={handleDocumentSelection} />
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        {filesUris.map((file, index) => {
+          return (
+            <TouchableOpacity
+              style={styles.itemContainer}
+              key={index.toString()}
+            >
+              <View
+                style={[
+                  styles.listItemDot,
+                  { backgroundColor: colorsP[index] },
+                ]}
+              />
+              <Text style={styles.fileName}>{file?.name}</Text>
+            </TouchableOpacity>
+          );
+        })}
+        <AppButton
+          style={styles.appButton}
+          titleStyle={{ fontSize: 12, color: colors.medium }}
+          title="Select 📑"
+          onPress={handleDocumentSelection}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -37,13 +81,34 @@ const DocumentPicker = () => {
 export default DocumentPicker;
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
+  appButton: {
+    width: 90,
+    padding: 7,
+    marginRight: 20,
+    backgroundColor: "transparent",
   },
-  uri: {
-    paddingBottom: 8,
-    paddingHorizontal: 18,
+  container: {
+    justifyContent: "center",
+    height: 40,
+    marginVertical: 10,
+  },
+  itemContainer: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    flexDirection: "row",
+    borderRadius: 40,
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  list: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  listItemDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    marginHorizontal: 7,
+    backgroundColor: "black",
   },
 });
