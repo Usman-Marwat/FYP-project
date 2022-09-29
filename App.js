9afe3525833a8313eb901cf2e3d62e55f1d9a1db
Usman-Marwat <us895907a2@gmail.com>
2022-09-29 18:33:29 +0500

    Build reusable map componenet

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
 import * as React from "react";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import useLocations from "./app/hooks/useLocations";
+import { StyleSheet } from "react-native";
+import MyMap from "./app/components/MyMap";
 
 export default function App() {
-  const { latitude, longitude } = useLocations();
-  return (
-    <View style={styles.container}>
-      {latitude && (
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          initialRegion={{
-            latitude,
-            longitude,
-            latitudeDelta: 0.017,
-            longitudeDelta: 0.017,
-          }}
-        >
-          <Marker
-            draggable
-            coordinate={{ latitude, longitude }}
-            onDragEnd={(e) => console.log(e.nativeEvent.coordinate)}
-          />
-        </MapView>
-      )}
-    </View>
-  );
+  return <MyMap />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ app/components/MyMap.js
@@ -0,0 +1,51 @@
+import React from "react";
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import useLocations from "../hooks/useLocations";
+
+const { width, height } = Dimensions.get("window");
+
+const MyMap = ({ initialRegion = null, onMarkerPress, onDragEnd }) => {
+  const { latitude, longitude } = useLocations();
+  const defaultRegion = {
+    latitude,
+    longitude,
+    latitudeDelta: 0.017,
+    longitudeDelta: 0.017,
+  };
+
+  return (
+    <View style={styles.mapContainer}>
+      {latitude && (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={initialRegion ? initialRegion : defaultRegion}
+        >
+          <Marker
+            draggable
+            coordinate={{ latitude, longitude }}
+            onDragEnd={(e) => onDragEnd(e.nativeEvent.coordinate)}
+            onPress={onMarkerPress}
+          >
+            <Callout>
+              <Text>Select an approximate location of site</Text>
+            </Callout>
+          </Marker>
+        </MapView>
+      )}
+    </View>
+  );
+};
+
+export default MyMap;
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    backgroundColor: "#fff",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
